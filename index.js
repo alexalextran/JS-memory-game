762c9e223c92a3b56b6f3d94588a96842c58e767
alexalextran <alextran8385@gmail.com>
2022-01-09 22:32:35 +1100

    code now sucessfully ensures the number of squares is equal to the difficulty without any duplicates

--- index.js
+++ index.js
@@ -1,9 +1,65 @@
-function startgame(){
-    var difficulty = 0.05
+var page = 0
+var box_chosen = []
 
-    for(let i = 0; i < 36; i++){
+function startgame(){
+    var difficulty = 20
+    
+
+    for(let i = 1; i <= 36; i++){
+
+        var box_selector = document.querySelector(`.box${i}`)
+        var condition = random()
+      
         
-    }
+       
+
+          breakme:  if (condition == difficulty){
+                console.log(i)
+                
+                if(box_chosen.indexOf(i) != -1){
+                    console.log('hehe haw')
+                    break breakme
+                }
+
+
+                
+               box_chosen.push(i)
+                box_selector.classList += " green"
+                page++
+              
+            }
+
+
+             if (page == difficulty){
+           
+            
+           
+            break
+        }
+
+            
+        }
+
+        
+           if(page != difficulty){
+                startgame()
+                
+            }
+
+     
 
     
+            console.log(box_chosen)
+    
 }
+
+function random(){
+var random = Math.ceil((Math.random()*36))
+
+
+    return random
+   
+
+
+}
+
