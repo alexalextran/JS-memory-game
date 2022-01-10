7caccfb3f1933191931b2b262fe1cbbd29e39d1f
alexalextran <alextran8385@gmail.com>
2022-01-10 21:24:35 +1100

    now once a user clicks on a box that was chosen the box will turn green else it will turn red

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 var page = 0
 var box_chosen = []
-
+var difficulty = 19
 
 function startgame(){
-box_random()
-document.body.classList += "pointer"
 
-console.log (box)
+
+box_random()
+
+
+
+
+    setTimeout(function()  {
+
+        cursor()
+     
+    },3500)
+
+
+
+
 console.log(box_chosen)
+
+
+
 }
 
 
 
+
 function box_random(){
-    var difficulty = 7
+  
     
 
     for(let i = 1; i <= 36; i++){
@@ -21,36 +37,30 @@ function box_random(){
         var box_selector = document.querySelector(`.box${i}`)
         var condition = random()
       
-        
-       
-
           breakme:  if (condition == difficulty){
-               
                 
                 if(box_chosen.indexOf(i) != -1){
                     console.log('hehe haw')
                     break breakme
                 }
 
-
                box_chosen.push(i)
-
                box_selector.style.animationName = "background"
-               
-                
                 page++
             }
 
-
              if (page == difficulty){
             break
-        }      
+        }  
+        
+        
+
         }
 
         
            if(page != difficulty){
-                startgame()
-                
+            box_random()
+        
             } 
 }
 
@@ -62,9 +72,27 @@ var random = Math.ceil((Math.random()*36))
 
 
     return random
-   
-
 
 }
 
 
+function cursor(){
+    document.body.classList += "pointer"
+    clearTimeout()
+}
+
+function check_box(event){
+var box_clicked_name = (event.target.className)
+var box_clicked_HTML = document.getElementsByClassName(`${box_clicked_name}`)
+var box_clicked = parseInt((event.target.className).slice(7))
+
+
+
+if (box_chosen.includes(box_clicked)){
+    box_clicked_HTML[0].style.backgroundColor = "rgb(0,167,28)"
+}
+else{
+    box_clicked_HTML[0].style.backgroundColor = "rgb(242,0,0)"
+}
+
+}
